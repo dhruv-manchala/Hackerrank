5fe417cbfcd6c04aa5d2b22a9651195ebc894833
Dhruv Manchala <dhruv.manchala.16@cmc.edu>
2014-11-08 01:55:34 -0800

    sherlock and watson

---
+++ Hackerrank C/Hackerrank C/array_utilities.h
@@ -0,0 +1,14 @@
+//
+//  array_utilities.h
+//  Hackerrank C
+//
+//  Created by Dhruv Manchala on 11/8/14.
+//  Copyright (c) 2014 Dhruv Manchala. All rights reserved.
+//
+
+#ifndef Hackerrank_C_array_utilities_h
+#define Hackerrank_C_array_utilities_h
+
+
+
+#endif
---
+++ Hackerrank C/Hackerrank C/sherlock_and_watson.c
@@ -0,0 +1,9 @@
+//
+//  sherlock_and_watson.c
+//  Hackerrank C
+//
+//  Created by Dhruv Manchala on 11/8/14.
+//  Copyright (c) 2014 Dhruv Manchala. All rights reserved.
+//
+
+#include <stdio.h>
