ebdaeca2f32388816de54e26744f522b18055710
Dhruv Manchala <dhruv.manchala.16@cmc.edu>
2014-11-08 00:11:45 -0800

    first commit

---
+++ Hackerrank C/service_lane.c
@@ -0,0 +1,99 @@
+//
+//  service_lane.c
+//  Hackerrank
+//
+//  Created by Dhruv Manchala on 11/7/14.
+//  Copyright (c) 2014 Dhruv Manchala. All rights reserved.
+//
+
+
+#include <stdio.h>
+
+
+// Ad Hoc Solution
+
+int main(void)
+{
+    int n;
+    int t;
+    
+    scanf("%d %d", &n, &t);
+    
+    int A[n];
+    
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &A[i]);
+    }
+    
+    int start;
+    int end;
+    
+    for (int i = 0; i < t; i++)
+    {
+        scanf("%d %d", &start, &end);
+        
+        int max_width = 3;
+        
+        for (int j = start; j <= end; j++)
+        {
+            max_width = A[j] < max_width ? A[j] : max_width;
+            
+            if (A[j] == 1) break;
+        }
+        
+        printf("%d\n", max_width);
+    }
+    
+    return 0;
+}
+
+// Dynamic Programming Solution
+
+/*
+int main(void)
+{
+    int n;
+    int t;
+    
+    scanf("%d %d", &n, &t);
+    
+    int A[n];
+    
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &A[i]);
+    }
+    
+    int cases[t][2];
+    
+    for (int i = 0; i < t; i++)
+    {
+        scanf("%d %d", &cases[i][0], &cases[i][1]);
+    }
+    
+    int partial[n + 1][n];
+    
+    for (int i = 0; i < n; i++) partial[1][i] = A[i];
+    for (int k = 2; k <= n; k++)
+    {
+        for (int i = 0; i < n - k + 1; i++)
+        {
+            int prev = partial[k - 1][i];
+            partial[k][i] = prev < A[i + k - 1] ? prev : A[i + k - 1];
+        }
+    }
+    
+    for (int i = 0; i < t; i++)
+    {
+        int start = cases[i][0];
+        int end = cases[i][1];
+        
+        printf("%d\n", partial[end - start + 1][start]);
+    }
+    
+    return 0;
+}
+*/
+
+
---
+++ Hackerrank Scala/src/ServiceLane.scala
@@ -0,0 +1,24 @@
+import scala.io.StdIn.readLine
+
+object Solution {
+  
+  def main(args: Array[String]) {
+
+	val t = readLine.split(" ")(1).toInt
+    val A = readLine.split(" ").map(_.toInt)
+    
+    for (i <- 0 to t - 1) {
+      val Array(start, end) = readLine.split(" ").map(_.toInt)
+      maxWidth(A, start, end, 3)
+    }
+  }
+              
+  def maxWidth(A: Array[Int], start: Int, end: Int, curr: Int) {
+  
+	if (start > end) println(curr)
+	else if (A(start) == 1)  println(1)
+	else if (curr > A(start)) maxWidth(A, start + 1, end, A(start))
+	else maxWidth(A, start + 1, end, curr)
+  }
+    
+}
