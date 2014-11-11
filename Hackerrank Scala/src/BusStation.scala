6682799a0a52e493e156c4419dabb7c08f6d2396
Dhruv Manchala <dhruv.manchala.16@cmc.edu>
2014-11-11 00:18:17 -0800

    bus station update - removed for loop

--- Hackerrank Scala/src/BusStation.scala
+++ Hackerrank Scala/src/BusStation.scala
@@ -3,6 +3,7 @@ import scala.io.StdIn.readLine
 object BusStation {
   
   def main(args: Array[String]) {
+    
     readLine
     val L = readLine.split(" ").map(_.toInt).toList
        
@@ -10,9 +11,7 @@ object BusStation {
     val max = L.max
     val sum = L.foldRight(0)(_ + _)
     
-    for (i <- max to sum by min) {
-      if (sum % i == 0) validBusSize(i, L, 0)
-    }
+    (max to sum by min).filter(sum % _ == 0).foreach(validBusSize(_, L, 0)) 
   }
   
   def validBusSize(size: Int, L: List[Int], curr: Int) {
